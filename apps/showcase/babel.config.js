58e2df46ea2542d5646529a28e13baac5216aa84
zila <rimsila.itc@gmail.com>
2025-11-07 23:35:18 +0700

    update

--- apps/showcase/babel.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-};
--- apps/showcase/lib/constants.ts
+++
@@ -1,5 +0,0 @@
-export const COMPONENTS = [
-  { slug: 'example-component', name: 'Example Component' },
-  { slug: 'example-block', name: 'Example Block' },
-  { slug: 'form', name: 'Form' },
-] as const;
